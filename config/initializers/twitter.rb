bb84936041ddd9968e7cfd460696f0849b38867b
nkmrshn <nkmrshn@gmail.com>
2010-09-30 06:38:32 +0900

    if faild to read config/twitter.rb, read from ENV

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,4 +1,13 @@
-twitter = YAML.load(File.read(Rails.root.to_s + "/config/twitter.yml"))
-NextHoliday::Application.config.twitter_consumer_key = twitter["consumer_key"]
-NextHoliday::Application.config.twitter_consumer_secret = twitter["consumer_secret"]
-NextHoliday::Application.config.twitter_id = twitter["twitter_id"]
+begin
+  twitter = YAML.load(File.read(Rails.root.to_s + "/config/twitter.yml"))
+rescue
+  twitter = {
+    "consumer_key" => ENV['CONSUMER_KEY'],
+    "consumer_secret" => ENV['CONSUMER_SECRET'],
+    "twitter_id" => ENV['TWITTER_ID']
+  }
+ensure
+  NextHoliday::Application.config.twitter_consumer_key = twitter["consumer_key"]
+  NextHoliday::Application.config.twitter_consumer_secret = twitter["consumer_secret"]
+  NextHoliday::Application.config.twitter_id = twitter["twitter_id"]
+end
