c2dac71351f66acbcc64c267fd325bff5432040c
nkmrshn <nkmrshn@gmail.com>
2010-10-12 06:32:44 +0900

    Fixed to controller to get timezone today

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    today = Date.today
+    today = Time.zone.today
     @last_day = Holiday.lastDay(today).first
     @next_day = Holiday.nextDay(today).first
   end
