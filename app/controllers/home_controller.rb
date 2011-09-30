a20c7f3925a68d0f1cc22805595dbce54cb81e39
nkmrshn <nkmrshn@gmail.com>
2011-10-01 00:25:39 +0900

    delete xml support

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,9 +3,5 @@ class HomeController < ApplicationController
     today = Time.zone.today
     @last_day = Holiday.lastDay(today).first
     @next_day = Holiday.nextDay(today).first
-    respond_to do |format|
-      format.html
-      format.xml { render :xml => [@last_day, @next_day] }
-    end
   end
 end
